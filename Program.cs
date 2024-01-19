3b8c6a77c6b1dc206a9d89acd98d6204543c0ecb
TigRig <tigrig.crex@gmail.com>
2024-01-20 00:34:40 +0900

    p13

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
 ﻿var dict = new Dictionary<int, int> { { 123, 111 }, { 456, 222 } };
 
-if (dict.TryGetValue(789, out var r))
+dict.TryExecute(789, Console.WriteLine);
+
+var dict2 = new Dictionary<string, int> { { "123", 111 }, { "456", 222 } };
+
+dict2.TryExecute("456", Console.WriteLine);
+
+public static class DictionaryExtension
 {
-    Console.WriteLine(r);
+    public static void TryExecute<TKey, TValue>(
+        this Dictionary<TKey, TValue> dict,
+        TKey key,
+        Action<TValue> action
+    )
+        where TKey : notnull
+    {
+        if (dict.TryGetValue(key, out var r))
+        {
+            action(r);
+        }
+    }
 }
