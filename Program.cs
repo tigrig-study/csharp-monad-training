b709eec34f5076c978f70d2bf5e1a0852637973c
TigRig <tigrig.crex@gmail.com>
2024-01-20 01:01:42 +0900

    冗長な連結処理を書いてみる

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
-﻿var valueHolder = new ValueHolder<string?>("123");
-valueHolder.TryExecute(Console.WriteLine);
+﻿var valueHolder1 = new ValueHolder<string?>("abc");
+var result1 = ""; // ← ここで定義するのがビミョー
+valueHolder1.TryExecute(value => result1 = value + "def");
 
-var valueHolder2 = new ValueHolder<string?>(null);
-valueHolder2.TryExecute(Console.WriteLine);
+var valueHolder2 = new ValueHolder<string?>(result1);
+var result2 = ""; // ← ここで定義するのがビミョー
+valueHolder2.TryExecute(value => result2 = value + "ghi");
+
+Console.WriteLine(result2);
