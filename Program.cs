7a0108a7752ae95ee555bfd37d254df78fc7da5b
TigRig <tigrig.crex@gmail.com>
2024-01-20 01:09:35 +0900

    p19

--- Program.cs
+++ Program.cs
@@ -5,3 +5,14 @@ var valueHolder2 = new ValueHolder<string?>(result1);
 var result2 = valueHolder2.TryExecute(value => value + "ghi");
 
 Console.WriteLine(result2);
+
+var valueHolder3 = new ValueHolder<string?>(null);
+var result3 = valueHolder3.TryExecute(value => value + "ghi");
+if (result3 is null)
+{
+    Console.WriteLine("is null!");
+}
+else
+{
+    Console.WriteLine(result3);
+}
