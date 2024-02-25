017ed114c7255a48c6ddfff59b362e18de680964
TigRig <tigrig.crex@gmail.com>
2024-02-25 10:57:51 +0900

    P50

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿var option11 = Option.Return(new int[] { 1 });
+﻿// Option.Bind
+var option11 = Option.Return(new int[] { 1 });
 var option12 = option11.Bind(value1 =>
 {
     var option21 = Option.Return(new int[] { 2 });
@@ -10,3 +11,17 @@ var option12 = option11.Bind(value1 =>
 });
 
 option12.Match(Console.WriteLine);
+
+// LINQ.SelectMany
+var list11 = new int[] { 1, 2 };
+var list12 = list11.SelectMany(value1 =>
+{
+    var list21 = new int[] { 2 };
+    return list21.SelectMany(value2 =>
+    {
+        var list31 = new int[] { 3 };
+        return list31.SelectMany(value3 => new string[] { "" + value1 + value2 + value3 });
+    });
+});
+
+Console.WriteLine(string.Join(", ", list12));
